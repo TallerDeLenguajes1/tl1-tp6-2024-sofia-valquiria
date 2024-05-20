1e3428708afdfa052b227bf332260a452f778d73
Sofia Sandoval <sandovalsofi99@gmail.com>
2024-05-20 16:11:00 -0300

    reiniciando dotnet console project

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int num;
-
-Console.WriteLine("Escriba un numero:");
-string cad = Console.ReadLine();
-if(int.TryParse(cad,out num)){
-    Console.Write(num);
-}
